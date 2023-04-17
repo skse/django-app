35fa43d70e1c3dd24bf6a618a54835b8f868dc1f
skse <viasorokin@yandex.ru>
2023-04-17 08:59:43 +0300

    add comments

--- project/up/views.py
+++ project/up/views.py
@@ -3,9 +3,17 @@ from django.http import HttpResponse
 
 
 def index(request):
+    """
+    Determines if app is available
+    """
+
     return HttpResponse("")
 
 
 def databases(request):
+    """
+    Determines if DB is up & running
+    """
+
     connection.ensure_connection()
     return HttpResponse("")
--- project/users/views.py
+++ project/users/views.py
@@ -5,6 +5,9 @@ from users.serializers import UserSerializer
 
 
 class UserManageView(RetrieveUpdateAPIView):
+    """
+    Returns relevant info for a currently logged user, and allows to manipulate its subscriptions state.
+    """
     serializer_class = UserSerializer
     http_method_names = ["get", "patch"]
 
