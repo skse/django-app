3ccc1e4a11066fa10465b60336f7c813d1cf6c92
skse <viasorokin@yandex.ru>
2023-04-17 09:38:11 +0300

    format

--- project/users/views.py
+++ project/users/views.py
@@ -8,6 +8,7 @@ class UserManageView(RetrieveUpdateAPIView):
     """
     Returns relevant info for a currently logged user, and allows to manipulate its subscriptions state.
     """
+
     serializer_class = UserSerializer
     http_method_names = ["get", "patch"]
 
